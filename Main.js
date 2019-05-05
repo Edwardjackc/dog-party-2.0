675405887d34b20f57d39c3b7a035857b07ed22f
Edward Cheatham <edwardjackc@gmail.com>
2019-05-05 14:57:29 -0600

    Move button over

--- Main.js
+++ Main.js
@@ -1,11 +1,19 @@
 var nameOutput = document.querySelector('#dog__name');
 var nameInput = document.querySelector('#name__input');
 var btn = document.querySelector('#btn__submit');
+var btnDog = document.querySelector(".btn__dogs")
 
 btn.addEventListener('click',nameUpdate)
+btnDog.addEventListener('click',toggleDisplayDog)
 
 function nameUpdate(e) {
   e.preventDefault();
   nameInput.value ? nameOutput.innerText=nameInput.value :"";
   nameInput.value =""
-  } 
+}
+
+function toggleDisplayDog(e) {
+  e.preventDefault(); 
+  btnDog ?e.target.parentNode.style.visibility="hidden":e.target.parentNode.style.visibility ="visible"
+  console.log("yo")
+}
